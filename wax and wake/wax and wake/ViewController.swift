2a0be73ab9e087e0c5759494466c8e76f34e37f7
emilyw26 <emilywu1456@gmail.com>
2018-03-27 10:10:38 -0400

    initial commit

---
+++ wax and wake/wax and wake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wax and wake
+//
+//  Created by Emily Wu on 3/27/18.
+//  Copyright © 2018 ramen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ wax and wake/wax and wakeTests/wax_and_wakeTests.swift
@@ -0,0 +1,36 @@
+//
+//  wax_and_wakeTests.swift
+//  wax and wakeTests
+//
+//  Created by Emily Wu on 3/27/18.
+//  Copyright © 2018 ramen. All rights reserved.
+//
+
+import XCTest
+@testable import wax_and_wake
+
+class wax_and_wakeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
